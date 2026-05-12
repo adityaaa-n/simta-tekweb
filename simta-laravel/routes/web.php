be52bf79d7cecaac7a61a3882857016b4fb9b0de
adityaaa-n <mhmdadtya5656@gmail.com>
2026-05-13 03:00:18 +0700

    resolve merge conflict koordinator routes

--- simta-laravel/routes/web.php
+++ simta-laravel/routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/koordinator/dashboard', function () {
     return view('koordinator.dashboard');
 });
+
+Route::get('/koordinator/verifikasi', function () {
+    return view('koordinator.verifikasi');
+});
+
+Route::get('/koordinator/penjadwalan', function () {
+    return view('koordinator.penjadwalan');
+});
+
+Route::get('/koordinator/manajemen-dosen', function () {
+    return view('koordinator.manajemen-dosen');
+});
